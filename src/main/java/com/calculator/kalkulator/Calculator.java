75026fdc53a7f3cd0aeb47a1e80e8740c0b63f65
“mattpolski <mattpolski@gmail.com>
2018-03-06 11:40:27 +0100

    Simple Calculator

---
+++ src/main/java/com/calculator/kalkulator/Calculator.java
@@ -0,0 +1,12 @@
+package com.calculator.kalkulator;
+
+public class Calculator {
+    public double substractAFromB (double a, double b) {
+
+        return a - b;
+    }
+
+    public int addAtoB (int a, int b) {
+        return a + b;
+    }
+}
